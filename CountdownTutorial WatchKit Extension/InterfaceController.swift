8f9f96fb074e4947b7be5315cb82f819847cd5b5
Chad Towle <chad@towlebooth.com>
2015-12-23 15:17:48 -0600

    Tutorial 1 complete.

--- CountdownTutorial WatchKit Extension/InterfaceController.swift
+++ CountdownTutorial WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,16 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var yearsLabel: WKInterfaceLabel!
+    @IBOutlet var monthsLabel: WKInterfaceLabel!
+    @IBOutlet var daysLabel: WKInterfaceLabel!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        // start/refresh countdown using a one-second timer
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self,
+            selector: Selector("startCountDown:"), userInfo: nil, repeats: true)
     }
 
     override func willActivate() {
@@ -28,4 +33,40 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func startCountDown(timer: NSTimer) {
+        // create calendar, date and formatter variables
+        let userCalendar = NSCalendar.currentCalendar()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.calendar = userCalendar
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        // create variable to hold 7 years
+        let sevenYears: NSDateComponents = NSDateComponents()
+        sevenYears.setValue(7, forComponent: NSCalendarUnit.Year);
+        
+        // hard-code start date value for part one of this tutorial
+        let startDateIntertech: NSDate = dateFormatter.dateFromString("2005-06-01")!
+        
+        // add 7 years to our start date to calculate the date of our sabbatical
+        var sabbaticalDate = userCalendar.dateByAddingComponents(sevenYears, toDate: startDateIntertech,
+            options: NSCalendarOptions(rawValue: 0))
+        
+        // since we get a sabbatical every 7 years, add 7 years until sabbaticalDate is in the future
+        while sabbaticalDate!.timeIntervalSinceNow.isSignMinus {
+            sabbaticalDate = userCalendar.dateByAddingComponents(sevenYears, toDate: sabbaticalDate!,
+                options: NSCalendarOptions(rawValue: 0))
+        }
+        
+        // create year, month and day values for display
+        let flags: NSCalendarUnit = [.Year, .Month, .Day]
+        let dateComponents = userCalendar.components(flags, fromDate: NSDate(), toDate: sabbaticalDate!, options: [])
+        let year = dateComponents.year
+        let month = dateComponents.month
+        let day = dateComponents.day
+        
+        // set labels on interface
+        yearsLabel.setText(String(format: "%d Years", year))
+        monthsLabel.setText(String(format: "%d Months", month))
+        daysLabel.setText(String(format: "%d Days", day))
+    }
 }
